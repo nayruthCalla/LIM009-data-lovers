005662e2c4c5fcc41397f95cb812c343908f99e4
nayruthCalla <nayruth.calla@gmail.com>
2019-03-15 23:00:07 -0500

    mostrando nombres e imagenes de la data pokemon.js

--- src/data.js
+++ src/data.js
@@ -3,8 +3,11 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
 
-window.example = example;
+// window.showDataPoke=showDataPoke;
+
+// const example = () => { 
+//   return 'example';
+// };
+
+// window.example = example;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 /* Manejo del DOM */
+const showPoke = (dataPoke) => {
+    return `
+	<div class="pokeEst">
+	<img class="img" src="${dataPoke.img}"></img>
+	<h2>${dataPoke.name}</h2>
+	</div>
+	`
+}
+
+document.getElementById('cont-main').innerHTML = ` <h1>Estos son los nombres del pokemon</h1>
+<div id="poke">  </div>
+${POKEMON.pokemon.map(showPoke).join(' ')}`
