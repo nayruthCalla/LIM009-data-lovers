21a6d49d49249ad8ff08fd137b4611b7c206387e
nayruthCalla <nayruth.calla@gmail.com>
2019-03-18 03:21:49 -0500

    agregando funcion ordenar y en html slide

--- src/data.js
+++ src/data.js
@@ -11,3 +11,57 @@
 // };
 
 // window.example = example;
+
+const data = Object.values(POKEMON.pokemon); //obeteniendo data en un array de todo el objeto POKEMON
+
+
+let sortData = (sortBy, sortOrder) => { //funcion ordenar
+
+    // let data= fullData();//recorremos con map todo el obejeto para obetenr el valor de propiedad nombre
+    if (sortBy === 'name' && sortOrder === 'a-z') {
+        data.sort((a, b) => a.name > b.name ? 1 : -1)
+
+        //  {
+
+        // 	if(a>b){
+        // 		return 1;
+        // 	}
+        // 	if(a<b){
+        // 		return -1;
+        // 	}else{
+        // 		return 0;
+        // 	}
+
+        // }
+        // )
+
+    } else if (sortBy === 'name' && sortOrder === 'z-a') {
+        data.sort((a, b) => a.name < b.name ? 1 : -1)
+        // 	{
+
+        // 	if(a<b){
+        // 		return 1;
+        // 	}
+        // 	if(a>b){
+        // 		return -1;
+        // 	}else{
+        // 		return 0;
+        // 	}
+
+        // }
+        // )
+
+    }
+    // console.log()
+    //let resu=data.map((el)=>el.name)
+    return data;
+}
+
+
+
+// console.log(sortData('name','a-z'));
+
+
+
+
+window.sortData = sortData;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 /* Manejo del DOM */
 const showPoke = (dataPoke) => {
-    return `
+	return `
 	<div class="pokeBox">
 	<img class="img" src="${dataPoke.img}"></img>
 	<h2>${dataPoke.name}</h2>
@@ -8,6 +8,31 @@ const showPoke = (dataPoke) => {
 	`
 }
 
-document.getElementById('cont-main').innerHTML = ` <h1>Estos son los nombres del pokemon</h1>
+document.getElementById('cont-data').innerHTML = ` <h3>Estos son los nombres del pokemon</h3>
 <div id="poke">  </div>
 ${POKEMON.pokemon.map(showPoke).join(' ')}`
+
+//utilizando la funcion sort ('name',''a-z)
+const idBtn = document.getElementById('cont-main');
+
+let namesPoke = sortData('name', 'z-a'); //llamando a la funcion sort declarada en  data.js
+
+const viewData = (view) => { //funcion que capata nombres e ímagnes de la data
+
+	document.getElementById('cont-order').innerHTML += ` 
+	<div class="pokeBox ">
+	 
+	<img class="img" src="${view.img}"></img>
+	<h2>${view.name}</h2> 
+	</div>
+
+
+	`
+}
+idBtn.addEventListener('click', e => {
+	document.getElementById('cont-data').style.display = 'none';
+
+	if (e.target.id === 'btn-order') {
+		namesPoke.forEach(viewData);
+	}
+});
