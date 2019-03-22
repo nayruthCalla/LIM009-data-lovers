57ef0be23eab8bee11447fe7477287391ae5e1ac
nayruthCalla <nayruth.calla@gmail.com>
2019-03-22 14:57:00 -0500

    plantilla principal que muestra la data y la funcion ordenar

--- src/main.js
+++ src/main.js
@@ -1,58 +1,30 @@
 /* Manejo del DOM */
 const valuesObjet = POKEMON.pokemon; //obeteniendo data en un array de todo el objeto POKEMON
-const showData = (view) => {
-	document.getElementById('cont-data').innerHTML += 
-	`
-	<object class='style-mold'>
-	<img src='${view.img}'/>
-	<param />${view.name}
-	</object>
-	` 
-}
-valuesObjet.forEach(showData);
+let valuesData = "";
+const showData = view => {
+  document.getElementById("cont-data").innerHTML = "";
+  view.forEach(element => {
+    valuesData = `
+		<object class='style-mold'>
+		<img src='${element.img}'/>
+		<param />${element.name}
+		</object>
+		`;
+    document.getElementById("cont-data").innerHTML += `${valuesData}`;
+  });
+};
+showData(valuesObjet);
 
-//filtrando por tipo
+const idBtn = document.getElementById("id-btn");
+let namesPoke = sortData(valuesObjet, "name", "a-z");
 
+idBtn.addEventListener("click", e => {
+  // document.getElementById('content1').style.display= 'none';
 
-
-
-
-// const showPoke = (dataPoke) => {
-// 	return `
-// 	<div class="">
-// 	<img class="img" src="${dataPoke.img}"></img>
-// 	<h2>${dataPoke.name}</h2>
-// 	</div>
-// 	`
-// }
-
-// document.getElementById('cont-data').innerHTML = ` <h3>Estos son los nombres del pokemon</h3>
-// <div id="poke">  </div>
-// ${POKEMON.pokemon.map(showPoke).join(' ')}`
-
-// //utilizando la funcion sort ('name',''a-z)
-// const idBtn = document.getElementById('cont-main');
-// const btn = document.getElementById("btn-order")
-
-// let namesPoke = sortData('name', 'z-a'); //llamando a la funcion sort declarada en  data.js
-
-// const viewData = (view) => { //funcion que capata nombres e ímagnes de la data
-
-// 	document.getElementById('cont-order').innerHTML += ` 
-// 	<div class="pokeBox ">
-
-// 	<img class="img" src="${view.img}"></img>
-// 	<h2>${view.name}</h2> 
-// 	</div>
-
-
-// 	`
-// }
-// btn.addEventListener('click', e => {
-// 	document.getElementById('cont-data').style.display = 'none';
-// 	console.log(e.target.id);
-// 	if (e.target.id === 'btn-order') {
-// 		namesPoke.forEach(viewData);
-
-// 	}
-// });
+  if (e.target.id === "btn-name") {
+    // document.getElementById("content2").innerHTML = `${namesPoke
+    //   .map(viewPoke)
+    //   .join(" ")}`;
+    showData(namesPoke);
+  }
+});
