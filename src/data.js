d720a04608c19c4fe36c2bc283b8bee8f5cca587
nayruthCalla <nayruth.calla@gmail.com>
2019-03-25 03:48:15 -0500

    boton ordenar y filtrar

--- src/data.js
+++ src/data.js
@@ -1,18 +1,20 @@
-
-const sortData = (data, sortBy, sortOrder) => //funcion ordenar
-{
-    if (sortBy === 'name' && sortOrder === 'a-z') {
-        data.sort((a, b) => a.name > b.name ? 1 : -1)
-    }
-    else
-        if (sortBy === 'name' && sortOrder === 'z-a') {
-            data.sort((a, b) => a.name < b.name ? 1 : -1)
-        }
-    return data;
-}
+const sortData = (
+  data,
+  sortBy,
+  sortOrder //funcion ordenar
+) => {
+  if (sortBy === "name" && sortOrder === "a-z") {
+    data.sort((a, b) => (a.name > b.name ? 1 : -1));
+  } else if (sortBy === "name" && sortOrder === "z-a") {
+    data.sort((a, b) => (a.name < b.name ? 1 : -1));
+  }
+  return data;
+};
 window.sortData = sortData;
 
-const filter=(arrDat, condition)=>{
-
-    
-}
+const filter = (arrDat, condition) => {
+  const filterType = arrDat.filter(
+    ele => ele.type[0] === condition || ele.type[1] === condition
+  );
+  return filterType;
+};
--- src/main.js
+++ src/main.js
@@ -7,27 +7,59 @@ const showData = view => {
     valuesData = `
     
       <div class="row cuatro-columnas"
-        <div class="col-xs-12 col-sm-6 col-ml-3">
+        <article class="col-xs-12 col-sm-6 col-ml-3">
           <img src='${element.img}'/>
-          <param />${element.name}
-           </div>
-	      	</object>
+          <h3 class='color-font' >${element.name}</h3>
+           </article>
+	      	</div>
 		`;
     document.getElementById("cont-data").innerHTML += `${valuesData}`;
   });
 };
 showData(valuesObjet);
+//filtrar
+var arrTipos = [];
+var arrTiposUnicos = [];
 
-const idBtn = document.getElementById("id-btn");
-let namesPoke = sortData(valuesObjet, "name", "a-z");
-
-idBtn.addEventListener("click", e => {
-  // document.getElementById('content1').style.display= 'none';
-
-  if (e.target.id === "btn-name") {
-    // document.getElementById("content2").innerHTML = `${namesPoke
-    //   .map(viewPoke)
-    //   .join(" ")}`;
-    showData(namesPoke);
+/* Tipo */
+for (let i = 0; i < valuesObjet.length; i++) {
+  // console.log(arrData[i].hasOwnProperty('type'))
+  if (valuesObjet[i].hasOwnProperty("type")) {
+    arrTipos.push(valuesObjet[i].type);
   }
+}
+for (let i = 0; i < arrTipos.length; i++) {
+  // console.log(arrTiposUnicos[i])
+  for (let j = 0; j < arrTipos[i].length; j++) {
+    // console.log(arrTiposUnicos.push(arrTipos[i][j]));
+    if (arrTiposUnicos.indexOf(arrTipos[i][j]) === -1) {
+      arrTiposUnicos.push(arrTipos[i][j]);
+      // arrTiposUnicos;
+    }
+  }
+}
+
+for (var i = 0; i < arrTiposUnicos.length; i++) {
+  document.getElementById("btn-filter").innerHTML += `	
+           <option>${arrTiposUnicos[i]}</option>
+           `;
+}
+//funcion filtrar
+let cont3 = document.getElementById("btn-filter");
+cont3.addEventListener("change", () => {
+  let filterData = filter(
+    valuesObjet,
+    document.getElementById("btn-filter").value
+  );
+  showData(filterData);
+});
+
+const idBtnOrder = document.getElementById("btn-order");
+idBtnOrder.addEventListener("change", () => {
+  let orderDataPoke = sortData(
+    valuesObjet,
+    "name",
+    document.getElementById("btn-order").value
+  );
+  showData(orderDataPoke);
 });
