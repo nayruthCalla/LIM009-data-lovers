81be220fdeba19bf7f2441ad98ed776f1cf83c22
wendy <wendy.medrano@unmsm.edu.pe>
2019-03-31 22:30:49 -0500

    Merging changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-/* Manejo del DOM */
-window.POKEMON;
-const valuesObjet= POKEMON.pokemon; 
-
+Window.POKEMON;
+const valuesObjet = POKEMON.pokemon;
 let valuesData = "";
 const showData = view => {
   document.getElementById("cont-data").innerHTML = "";
@@ -25,12 +23,14 @@ var arrTiposUnicos = [];
 
 /* Tipo */
 for (let i = 0; i < valuesObjet.length; i++) {
-  // console.log(arrData[i].hasOwnProperty('type'))
+  // console.log(valuesObjet[i].hasOwnProperty('type'));
   if (valuesObjet[i].hasOwnProperty("type")) {
     arrTipos.push(valuesObjet[i].type);
   }
 }
+
 for (let i = 0; i < arrTipos.length; i++) {
+  // console.log(arrTipos.length);
   // console.log(arrTiposUnicos[i])
   for (let j = 0; j < arrTipos[i].length; j++) {
   
@@ -50,14 +50,22 @@ for (var i = 0; i < arrTiposUnicos.length; i++) {
 
 let cont3 = document.getElementById("btn-filter");
 cont3.addEventListener("change", () => {
-  showData(filterData(valuesObjet, document.getElementById("btn-filter").value)
+  showData(
+    GlobalPoke.filterData(
+      valuesObjet,
+      document.getElementById("btn-filter").value
+    )
   );
 });
 
 const idBtnOrder = document.getElementById("btn-order");
 idBtnOrder.addEventListener("change", () => {
   showData(
-    sortData(valuesObjet, "name", document.getElementById("btn-order").value)
+    GlobalPoke.sortData(
+      valuesObjet,
+      "name",
+      document.getElementById("btn-order").value
+    )
   );
 });
 
@@ -65,6 +73,4 @@ idBtnOrder.addEventListener("change", () => {
 // document.getElementById("stata").innerHTML = "";
 document.getElementById("stata").innerHTML += `
 <h4> ¿Sabias que el peso promedio de estos Pokémones es 
-${computeStats(valuesObjet)} kg. </h4>`;
-
-
+${GlobalPoke.computeStats(valuesObjet)} kg. </h4>`;
